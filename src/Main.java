1b54571bb1cf29efbde369c1840eba5032216375
bujorgheorghe <56941435+bujorgheorghe@users.noreply.github.com>
2019-10-25 16:57:01 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,9 @@
-import javax.sound.midi.Soundbank;
-import java.sql.SQLOutput;
-
 public class Main {
 
     public static void main(String[] args) {
         // 1.Create a loop that will print numbers from 1 to 10, except number 7
-        for (int N = 1; N <= 10; N++) {
-            if (N == 7) continue;
+        for (int n = 1; n <= 10; N++) {
+            if (n == 7) continue;
             System.out.println(N);
         }
         //2.Create An Array of integers from 1 to 5, Call an integer on array using index and print it.
