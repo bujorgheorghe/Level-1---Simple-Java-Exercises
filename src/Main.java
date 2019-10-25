bbb402b3a5d6129566c3ec8b59ce29ad9a72814d
bujorgheorghe <56941435+bujorgheorghe@users.noreply.github.com>
2019-10-25 17:08:29 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,9 @@ public class Main {
 
     public static void main(String[] args) {
         // 1.Create a loop that will print numbers from 1 to 10, except number 7
-        for (int n = 1; n <= 10; N++) {
+        for (int n = 1; n <= 10; n++) {
             if (n == 7) continue;
-            System.out.println(N);
+            System.out.println(n);
         }
         //2.Create An Array of integers from 1 to 5, Call an integer on array using index and print it.
         int[] array = {1, 2, 3, 4, 5};
